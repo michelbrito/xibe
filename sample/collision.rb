f999c20417a16a164603cda7baa847987794feec
Michel Brito <micheljunio@gmail.com>
2009-04-20 16:38:51 -0300

    Collision sample

---
+++ sample/collision.rb
@@ -0,0 +1,43 @@
+require 'xibe'
+include Xibe
+
+class Example < Application
+  def initialize
+    super(320,240)
+    self.fill = Color.white
+    @ball = Sprite.new("ball.bmp",32,32,true)
+    @clone = @ball.clone
+    @clone.pos(100,100)
+    add @ball, @clone
+  end
+
+  def update
+    if Key.press? K_RIGHT
+      @ball.x += 5
+      while(@ball.collide_with?(@clone))
+        @ball.x -= 1
+      end
+    end
+    if Key.press? K_LEFT
+      @ball.x -= 5
+      while(@ball.collide_with?(@clone))
+        @ball.x += 1
+      end
+    end
+    if Key.press? K_DOWN
+      @ball.y += 5
+      while(@ball.collide_with?(@clone))
+        @ball.y -= 1
+      end
+    end
+    if Key.press? K_UP
+      @ball.y -= 5
+      while(@ball.collide_with?(@clone))
+        @ball.y += 1
+      end
+    end
+   
+  end
+end
+
+Example.new.run
